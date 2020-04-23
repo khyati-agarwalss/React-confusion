df863d89d6353db3c6d911e0cf2ba3fc4dd2516f
Khyati Agarwal <khyati.agarwalss@gmail.com>
2020-04-23 23:20:47 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,7 +19,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     if(comments != null) {
         return (
             <div className="col-12 col-md-5 m-1">
@@ -36,14 +36,14 @@ function RenderComments({comments, addComment, dishId}) {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     } else {
         return (
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }
@@ -81,7 +81,7 @@ function RenderComments({comments, addComment, dishId}) {
                     </div>
                     <div className="row">
                         <RenderDish dish = {props.dish} />
-                        <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} /> 
+                        <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} /> 
                     </div>
                 </div>
             );
@@ -117,7 +117,7 @@ export class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
